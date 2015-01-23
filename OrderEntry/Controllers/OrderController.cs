f8e682364f898b0cb357cd58a55b6ee422d73764
dhill <doughill1000@gmail.com>
2015-01-23 09:08:35 -0500

    Create View layout for Orders

---
+++ OrderEntry/Controllers/OrderController.cs
@@ -0,0 +1,128 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Linq;
+using System.Net;
+using System.Web;
+using System.Web.Mvc;
+using OrderEntry.Models;
+
+namespace OrderEntry.Controllers
+{
+    public class OrderController : Controller
+    {
+        private OrderEntryEntities db = new OrderEntryEntities();
+
+        // GET: /Order/
+        public ActionResult Index()
+        {
+            return View(db.Orders.ToList());
+        }
+
+        // GET: /Order/Details/5
+        public ActionResult Details(Guid? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Order order = db.Orders.Find(id);
+            if (order == null)
+            {
+                return HttpNotFound();
+            }
+            return View(order);
+        }
+
+        // GET: /Order/Create
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: /Order/Create
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // more details see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create([Bind(Include="ID,ODATE,SDATE,CUSTNO,CUSTNA,CORDNO,OSTATUS,SPERSON1,SNAME1,SPERSON2,SNAME2,CBADR1,CBADR2,CBADR3,CBCITY,CBST,CBZIP,CNTRYNO,COUNTRY,CBPHONE,CSNAME,CSADR1,CSADR2,CSADR3,CSCITY,CSST,CSZIP,CSCOUNTRY,CSATTN,XPORT,XRATE,TERMNO,TERMS,CURRENCY,LABPRT,OCONPRT,OHSTATUS,REMARKS,DELIVERY_TERMS,SHIP_VIA,MARKET,OFFICE_REMARKS,NOT_BEFORE,AUTHORIZATION")] Order order)
+        {
+            if (ModelState.IsValid)
+            {
+                order.ID = Guid.NewGuid();
+                db.Orders.Add(order);
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+
+            return View(order);
+        }
+
+        // GET: /Order/Edit/5
+        public ActionResult Edit(Guid? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Order order = db.Orders.Find(id);
+            if (order == null)
+            {
+                return HttpNotFound();
+            }
+            return View(order);
+        }
+
+        // POST: /Order/Edit/5
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // more details see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit([Bind(Include="ID,ODATE,SDATE,CUSTNO,CUSTNA,CORDNO,OSTATUS,SPERSON1,SNAME1,SPERSON2,SNAME2,CBADR1,CBADR2,CBADR3,CBCITY,CBST,CBZIP,CNTRYNO,COUNTRY,CBPHONE,CSNAME,CSADR1,CSADR2,CSADR3,CSCITY,CSST,CSZIP,CSCOUNTRY,CSATTN,XPORT,XRATE,TERMNO,TERMS,CURRENCY,LABPRT,OCONPRT,OHSTATUS,REMARKS,DELIVERY_TERMS,SHIP_VIA,MARKET,OFFICE_REMARKS,NOT_BEFORE,AUTHORIZATION")] Order order)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Entry(order).State = EntityState.Modified;
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            return View(order);
+        }
+
+        // GET: /Order/Delete/5
+        public ActionResult Delete(Guid? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Order order = db.Orders.Find(id);
+            if (order == null)
+            {
+                return HttpNotFound();
+            }
+            return View(order);
+        }
+
+        // POST: /Order/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public ActionResult DeleteConfirmed(Guid id)
+        {
+            Order order = db.Orders.Find(id);
+            db.Orders.Remove(order);
+            db.SaveChanges();
+            return RedirectToAction("Index");
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                db.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+    }
+}
---
+++ OrderEntry/Models/Metadata.cs
@@ -0,0 +1,80 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace OrderEntry.Models
+{
+    public class OrderMetadata
+    {
+        [DisplayName("Created Date")]
+        [DisplayFormat(DataFormatString = "{0:MM/dd}")]
+        public Nullable<System.DateTime> ODATE { get; set; }
+
+        [Required]
+        [DisplayName("Ship Date")]
+        [DisplayFormat(DataFormatString = "{0:MM/dd}")]
+        public Nullable<System.DateTime> SDATE { get; set; }
+
+        [Required]
+        [DisplayName("Customer Name")]
+        [MaxLength(30, ErrorMessage = "Maximum length is 30 characters.")]
+        public string CUSTNA { get; set; }
+
+        [Required]
+        [DisplayName("Customer Order#")]
+        [Range(0, int.MaxValue, ErrorMessage = "Please enter valid integer Number")]
+        public string CORDNO { get; set; }
+
+        [Required]
+        [DisplayName("Sales Rep")]
+        [MaxLength(20, ErrorMessage = "Maximum length is 20 characters.")]
+        public string SNAME1 { get; set; }
+
+        [Required]
+        [DisplayName("Customer Name")]
+        [MaxLength(25, ErrorMessage = "Maximum length is 25 characters.")]
+        public string CSNAME { get; set; } 
+
+        [Required]
+        [DisplayName("Address 1")]
+        [MaxLength(25, ErrorMessage = "Maximum length is 25 characters.")]
+        public string CSADR1 { get; set; }
+
+        [DisplayName("Address 2")]
+        [MaxLength(25, ErrorMessage = "Maximum length is 25 characters.")]
+        public string CSADR2 { get; set; }
+
+        [DisplayName("Address 3")]
+        [MaxLength(25, ErrorMessage = "Maximum length is 25 characters.")]
+        public string CSADR3 { get; set; }
+
+        [DisplayName("City")]
+        [MaxLength(20, ErrorMessage = "Maximum length is 20 characters.")]
+        public string CSCITY { get; set; }
+
+        [DisplayName("State")]
+        [MaxLength(2, ErrorMessage = "Maximum length is 2 characters.")]
+        public string CSST { get; set; }
+
+        [DisplayName("Zip Code")]
+        [MaxLength(5, ErrorMessage = "Maximum length is 5 characters.")]
+        public string CSZIP { get; set; }
+
+        [Required]
+        [DisplayName("Country")]
+        [MaxLength(20, ErrorMessage = "Maximum length is 20 characters.")]
+        public string CSCOUNTRY { get; set; }
+
+        [DisplayName("Attn")]
+        [MaxLength(20, ErrorMessage = "Maximum length is 20 characters.")]
+        public string CSATTN { get; set; }
+
+        [DisplayName("Special Instructions")]
+        [DataType(DataType.MultilineText)]
+        [MaxLength(500, ErrorMessage = "Maximum length is 500 characters.")]
+        public string REMARKS { get; set; }
+    }
+}
