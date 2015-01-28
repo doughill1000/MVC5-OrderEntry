39d0305989c7711c2659e5fe86a33aafec750f14
dhill <doughill1000@gmail.com>
2015-01-28 10:12:59 -0500

    Added Missing FIles

---
+++ OrderEntry/Models/Repository/OrderEntryRepository.cs
@@ -0,0 +1,118 @@
+﻿
+using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Data.Entity.SqlServer;
+using System.Linq;
+using System.Web.Mvc;
+
+
+namespace OrderEntry.Models.Repository
+{
+    /// <summary>
+    /// This class is used for managing orders
+    /// </summary>
+    public class OrderEntryRepository : IOrderEntryRepository
+    {
+        /// <summary>
+        /// The database used to manage orders
+        /// </summary>
+        private OrderEntryEntities db;
+
+        /// <summary>
+        /// Constructs a new order repository
+        /// </summary>
+        public OrderEntryRepository()
+        {
+            db = new OrderEntryEntities();
+
+            db.Database.Log = (s => System.Diagnostics.Debug.WriteLine(s));
+        }
+
+        /// <summary>
+        /// Creates a new order
+        /// </summary>
+        /// <param name="order">The order to create</param>
+        public void Create(Order order)
+        {
+            order.ID = Guid.NewGuid();
+
+            order.ODATE = DateTime.Now;
+
+            db.Orders.Add(order);
+
+            db.SaveChanges();
+        }
+
+        /// <summary>
+        /// Deletes an order with the specified ID
+        /// </summary>
+        /// <param name="id">The order ID</param>
+        public void Delete(Guid? id)
+        {
+            Order order = db.Orders.Find(id);
+
+            db.Orders.Remove(order);
+
+            db.SaveChanges();
+        }
+
+        /// <summary>
+        /// Manages the disposal of the class
+        /// </summary>
+        public void Dispose()
+        {
+            db.Dispose();
+        }
+
+        /// <summary>
+        /// Gets a list of all orders
+        /// </summary>
+        /// <returns>A list of all orders</returns>
+        public List<Order> GetAll()
+        {
+            List<Order> orders = db.Orders.ToList();
+
+            return orders;
+        }
+
+        /// <summary>
+        /// Returns order with the specified ID 
+        /// </summary>
+        /// <param name="Id">The order with the specified ID</param>
+        /// <returns></returns>
+        public Order GetById(Guid? Id)
+        {
+            Order order = db.Orders.Find(Id);
+
+            return order;
+        }
+
+        public void Update(Order order)
+        {
+            db.Entry(order).State = EntityState.Modified;
+
+            db.SaveChanges();
+        }
+        /// <summary>
+        /// Returns a list of Sales Persons
+        /// </summary>
+        /// <returns>List of Sales Persons</returns>
+        public List<SelectListItem> GetSalesPersons()
+        {
+            var sales = db.SalesPersons
+                .ToList()
+                .Select(s => new SelectListItem
+                {
+                    Value = s.Id.ToString(),
+                    Text = s.Name
+
+                }).OrderBy(c => c.Text).ToList();
+
+            return sales;
+
+            throw new NotImplementedException();
+        } 
+
+    }
+}
---
+++ OrderEntry/Models/Repository/TransprintRepository.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity.SqlServer;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace OrderEntry.Models.Repository
+{
+    public class TransprintRepository : ITransprintRepository
+    {
+
+        private TransprintEntities db;
+
+        /// <summary>
+        /// Constructs a new order repository
+        /// </summary>
+        public TransprintRepository()
+        {
+            db = new TransprintEntities();
+
+            db.Database.Log = (s => System.Diagnostics.Debug.WriteLine(s));
+        }
+
+        /// <summary>
+        /// Gets a list of all Customers for the dropdown on the Create View for Orders.
+        /// 999 indicates a deactivated customer, and customers above 4000 are not used
+        /// </summary>
+        /// <returns>A list of customers</returns>
+        public List<SelectListItem> GetCustomers()
+        {
+            var customers = db.Customers
+                .ToList()
+                .Where(c => c.SPERSON1 != "999" && c.CUSTNO < 4000)
+                .Select(c => new SelectListItem
+            {
+                Value = c.CUSTNO.ToString().Trim(),
+                Text = c.CUSTNA, 
+                
+            }).OrderBy(c => c.Text).ToList();
+
+            return customers;
+
+            throw new NotImplementedException();
+        }
+
+        public List<SelectListItem> GetCountries()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
