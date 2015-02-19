86425fda9f9cf131fb2885878c0d55ad0cd54216
dhill <doughill1000@gmail.com>
2015-02-19 14:39:50 -0500

    Initial Commit SeparationViewModel

---
+++ OrderEntry/Models/Orders/SeparationViewModel.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace OrderEntry.Models.Orders
+{
+    public class SeparationViewModel
+    {
+        public Separation Separation { get; set; }
+
+        public List<Separation> Separations { get; set; }
+    }
+}
